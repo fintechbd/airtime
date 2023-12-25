e794b7b6c20cea2f0231a667df0a14bad44f8ab6
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2023-12-26 03:38:41 +0600

    LP-186 store done without test

--- src/Http/Controllers/InternationalTopUpController.php
+++ src/Http/Controllers/InternationalTopUpController.php
@@ -2,6 +2,10 @@
 
 namespace Fintech\Airtime\Http\Controllers;
 use Exception;
+use Fintech\Business\Facades\Business;
+use Fintech\Core\Enums\Auth\RiskProfile;
+use Fintech\Core\Enums\Auth\SystemRole;
+use Fintech\Core\Enums\Transaction\OrderStatus;
 use Fintech\Core\Exceptions\StoreOperationException;
 use Fintech\Core\Exceptions\UpdateOperationException;
 use Fintech\Core\Exceptions\DeleteOperationException;
@@ -18,6 +22,7 @@ use Fintech\Transaction\Facades\Transaction;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\DB;
 
 /**
  * Class InternationalTopUpController
@@ -74,21 +79,107 @@ class InternationalTopUpController extends Controller
      */
     public function store(StoreInternationalTopUpRequest $request): JsonResponse
     {
+        DB::beginTransaction();
         try {
             $inputs = $request->validated();
-
-            $internationalTopUp = Airtime::internationalTopUp()->create($inputs);
-
-            if (!$internationalTopUp) {
-                throw (new StoreOperationException)->setModel(config('fintech.airtime.international_top_up_model'));
+            if ($request->input('user_id') > 0) {
+                $user_id = $request->input('user_id');
             }
+            $depositor = $request->user('sanctum');
+            if (Transaction::orderQueue()->addToQueueUserWise(($user_id ?? $depositor->getKey())) > 0) {
 
-            return $this->created([
-                'message' => __('core::messages.resource.created', ['model' => 'International Top Up']),
-                'id' => $internationalTopUp->id
-             ]);
+                $depositAccount = \Fintech\Transaction\Facades\Transaction::userAccount()->list([
+                    'user_id' => $user_id ?? $depositor->getKey(),
+                    'country_id' => $request->input('source_country_id', $depositor->profile?->country_id),
+                ])->first();
 
+                if (! $depositAccount) {
+                    throw new Exception("User don't have account deposit balance");
+                }
+
+                $masterUser = \Fintech\Auth\Facades\Auth::user()->list([
+                    'role_name' => SystemRole::MasterUser->value,
+                    'country_id' => $request->input('source_country_id', $depositor->profile?->country_id),
+                ])->first();
+
+                if (! $masterUser) {
+                    throw new Exception('Master User Account not found for '.$request->input('source_country_id', $depositor->profile?->country_id).' country');
+                }
+
+                //set pre defined conditions of deposit
+                $inputs['transaction_form_id'] = Transaction::transactionForm()->list(['code' => 'International_top_up'])->first()->getKey();
+                $inputs['user_id'] = $user_id ?? $depositor->getKey();
+                $delayCheck = Transaction::order()->transactionDelayCheck($inputs);
+                if ($delayCheck['countValue'] > 0) {
+                    throw new Exception('Your Request For This Amount Is Already Submitted. Please Wait For Update');
+                }
+                $inputs['sender_receiver_id'] = $masterUser->getKey();
+                $inputs['is_refunded'] = false;
+                $inputs['status'] = OrderStatus::Successful->value;
+                $inputs['risk'] = RiskProfile::Low->value;
+                $inputs['reverse'] = true;
+                $inputs['order_data']['currency_convert_rate'] = Business::currencyRate()->convert($inputs);
+                unset($inputs['reverse']);
+                $inputs['converted_amount'] = $inputs['order_data']['currency_convert_rate']['converted'];
+                $inputs['converted_currency'] = $inputs['order_data']['currency_convert_rate']['output'];
+                $inputs['order_data']['created_by'] = $depositor->name;
+                $inputs['order_data']['created_by_mobile_number'] = $depositor->mobile;
+                $inputs['order_data']['created_at'] = now();
+                $inputs['order_data']['master_user_name'] = $masterUser['name'];
+                //$inputs['order_data']['operator_short_code'] = $request->input('operator_short_code', null);
+                $inputs['order_data']['system_notification_variable_success'] = 'international_top_up_success';
+                $inputs['order_data']['system_notification_variable_failed'] = 'international_top_up_failed';
+
+                $internationalTopUp = Airtime::internationalTopUp()->create($inputs);
+
+                if (! $internationalTopUp) {
+                    throw (new StoreOperationException)->setModel(config('fintech.airtime.international_top_up_model'));
+                }
+
+                $order_data = $internationalTopUp->order_data;
+                $order_data['purchase_number'] = entry_number($internationalTopUp->getKey(), $internationalTopUp->sourceCountry->iso3, OrderStatus::Successful->value);
+                $order_data['service_stat_data'] = Business::serviceStat()->serviceStateData($internationalTopUp);
+                //TODO Need to work negative amount
+                $order_data['user_name'] = $internationalTopUp->user->name;
+                $internationalTopUp->order_data = $order_data;
+                $userUpdatedBalance = Airtime::internationalTopUp()->debitTransaction($internationalTopUp);
+                $depositedAccount = \Fintech\Transaction\Facades\Transaction::userAccount()->list([
+                    'user_id' => $depositor->getKey(),
+                    'country_id' => $internationalTopUp->source_country_id,
+                ])->first();
+                //update User Account
+                $depositedUpdatedAccount = $depositedAccount->toArray();
+                $depositedUpdatedAccount['user_account_data']['spent_amount'] = (float) $depositedUpdatedAccount['user_account_data']['spent_amount'] + (float) $userUpdatedBalance['spent_amount'];
+                $depositedUpdatedAccount['user_account_data']['available_amount'] = (float) $userUpdatedBalance['current_amount'];
+                if (((float) $depositedUpdatedAccount['user_account_data']['available_amount']) >= ((float) $userUpdatedBalance['spent_amount'])) {
+                    throw new Exception(__('Insufficient balance!', [
+                        'previous_amount' => ((float) $depositedUpdatedAccount['user_account_data']['available_amount']),
+                        'current_amount' => ((float) $userUpdatedBalance['spent_amount']),
+                    ]));
+                }
+                $order_data['order_data']['previous_amount'] = (float) $depositedAccount->user_account_data['available_amount'];
+                $order_data['order_data']['current_amount'] = (float) $userUpdatedBalance['current_amount'];
+                if (! Transaction::userAccount()->update($depositedAccount->getKey(), $depositedUpdatedAccount)) {
+                    throw new Exception(__('User Account Balance does not update', [
+                        'previous_amount' => ((float) $depositedUpdatedAccount['user_account_data']['available_amount']),
+                        'current_amount' => ((float) $userUpdatedBalance['spent_amount']),
+                    ]));
+                }
+                Airtime::internationalTopUp()->update($internationalTopUp->getKey(), ['order_data' => $order_data, 'order_number' => $order_data['purchase_number']]);
+                Transaction::orderQueue()->removeFromQueueUserWise($user_id ?? $depositor->getKey());
+                DB::commit();
+
+                return $this->created([
+                    'message' => __('core::messages.resource.created', ['model' => 'International Top Up']),
+                    'id' => $internationalTopUp->id,
+                    'spent' => $userUpdatedBalance['spent_amount'],
+                ]);
+            } else {
+                throw new Exception('Your another order is in process...!');
+            }
         } catch (Exception $exception) {
+            Transaction::orderQueue()->removeFromQueueUserWise($user_id ?? $depositor->getKey());
+            DB::rollBack();
 
             return $this->failed($exception->getMessage());
         }
