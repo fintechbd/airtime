1f0a67497ad057621fe2147d4b09890325a87ed6
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2023-12-26 03:40:41 +0600

    LP-186 crud done

---
+++ src/Repositories/Mongodb/InternationalTopUpRepository.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Fintech\Airtime\Repositories\Mongodb;
+
+use Fintech\Core\Repositories\MongodbRepository;
+use Fintech\Airtime\Interfaces\InternationalTopUpRepository as InterfacesInternationalTopUpRepository;
+use Illuminate\Contracts\Pagination\Paginator;
+use Illuminate\Database\Eloquent\Collection;
+use MongoDB\Laravel\Eloquent\Model;
+use InvalidArgumentException;
+
+/**
+ * Class InternationalTopUpRepository
+ * @package Fintech\Airtime\Repositories\Mongodb
+ */
+class InternationalTopUpRepository extends MongodbRepository implements InterfacesInternationalTopUpRepository
+{
+    public function __construct()
+    {
+       $model = app(config('fintech.airtime.international_top_up_model', \Fintech\Airtime\Models\InternationalTopUp::class));
+
+       if (!$model instanceof Model) {
+           throw new InvalidArgumentException("Mongodb repository require model class to be `MongoDB\Laravel\Eloquent\Model` instance.");
+       }
+
+       $this->model = $model;
+    }
+
+    /**
+     * return a list or pagination of items from
+     * filtered options
+     *
+     * @return Paginator|Collection
+     */
+    public function list(array $filters = [])
+    {
+        $query = $this->model->newQuery();
+
+        //Searching
+        if (! empty($filters['search'])) {
+            if (is_numeric($filters['search'])) {
+                $query->where($this->model->getKeyName(), 'like', "%{$filters['search']}%");
+            } else {
+                $query->where('name', 'like', "%{$filters['search']}%");
+                $query->orWhere('international_top_up_data', 'like', "%{$filters['search']}%");
+            }
+        }
+
+        //Display Trashed
+        if (isset($filters['trashed']) && $filters['trashed'] === true) {
+            $query->onlyTrashed();
+        }
+
+        //Handle Sorting
+        $query->orderBy($filters['sort'] ?? $this->model->getKeyName(), $filters['dir'] ?? 'asc');
+
+        //Execute Output
+        return $this->executeQuery($query, $filters);
+
+    }
+}
