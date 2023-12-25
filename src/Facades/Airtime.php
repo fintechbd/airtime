466a82c32817efc563a3a690c8ba2f5fdfc23194
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2023-12-26 02:30:12 +0600

    LP-186 packages facades class done

--- src/Facades/Airtime.php
+++ src/Facades/Airtime.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Facade;
 
 /**
  * @method static \Fintech\Airtime\Services\BangladeshTopUpService bangladeshTopUp()
+ * @method static \Fintech\Airtime\Services\InternationalTopUpService internationalTopUp()
  * // Crud Service Method Point Do not Remove //
  *
  * @see \Fintech\Airtime\Airtime
