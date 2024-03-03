7919c0fc56af7ec758d05590a333af8eff59f1ee
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-03-03 16:27:36 +0600

    typo fixed

--- src/Interfaces/BangladeshTopUpRepository.php
+++ src/Interfaces/BangladeshTopUpRepository.php
@@ -2,11 +2,12 @@
 
 namespace Fintech\Airtime\Interfaces;
 
+
 use Illuminate\Contracts\Pagination\Paginator;
-use Illuminate\Database\Eloquent\Model as EloquentModel;
+use Fintech\Core\Abstracts\BaseModel;
 use Illuminate\Support\Collection;
 use InvalidArgumentException;
-use MongoDB\Laravel\Eloquent\Model as MongodbModel;
+
 
 /**
  * Interface BangladeshTopUpRepository
@@ -24,14 +25,14 @@ interface BangladeshTopUpRepository
     /**
      * Create a new entry resource
      *
-     * @return EloquentModel|MongodbModel|null
+     * @return BaseModel
      */
     public function create(array $attributes = []);
 
     /**
      * find and update a resource attributes
      *
-     * @return EloquentModel|MongodbModel|null
+     * @return BaseModel
      */
     public function update(int|string $id, array $attributes = []);
 
@@ -39,7 +40,7 @@ interface BangladeshTopUpRepository
      * find and delete a entry from records
      *
      * @param  bool  $onlyTrashed
-     * @return EloquentModel|MongodbModel|null
+     * @return BaseModel
      */
     public function find(int|string $id, $onlyTrashed = false);
 
