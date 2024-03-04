7ceba76b36e0990b80f1680ba7f4e459885074a0
hafijul233 <hafijul233@users.noreply.github.com>
2024-03-04 20:09:22 +0000

    Fix styling

--- src/Interfaces/BangladeshTopUpRepository.php
+++ src/Interfaces/BangladeshTopUpRepository.php
@@ -15,34 +15,23 @@ interface BangladeshTopUpRepository
     /**
      * return a list or pagination of items from
      * filtered options
-     *
-     * @param array $filters
-     * @return Paginator|Collection
      */
     public function list(array $filters = []): Paginator|Collection;
 
     /**
      * Create a new entry resource
-     *
-     * @param array $attributes
-     * @return BaseModel
      */
     public function create(array $attributes = []): BaseModel;
 
     /**
      * find and update a resource attributes
-     *
-     * @param int|string $id
-     * @param array $attributes
-     * @return BaseModel
      */
     public function update(int|string $id, array $attributes = []): BaseModel;
 
     /**
      * find and delete a entry from records
      *
-     * @param int|string $id
-     * @param bool $onlyTrashed
+     * @param  bool  $onlyTrashed
      * @return ?BaseModel
      */
     public function find(int|string $id, $onlyTrashed = false): ?BaseModel;
