365f9d38cbfff1540bcdd8613e74aff25d5ee3e0
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-05-09 15:38:55 +0600

    trait replaced with macro

--- src/AirtimeServiceProvider.php
+++ src/AirtimeServiceProvider.php
@@ -3,6 +3,8 @@
 namespace Fintech\Airtime;
 
 use Fintech\Airtime\Commands\InstallCommand;
+use Fintech\Airtime\Providers\EventServiceProvider;
+use Fintech\Airtime\Providers\RepositoryServiceProvider;
 use Fintech\Core\Traits\RegisterPackageTrait;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,11 +22,11 @@ class AirtimeServiceProvider extends ServiceProvider
         $this->packageCode = 'airtime';
 
         $this->mergeConfigFrom(
-            __DIR__.'/../config/airtime.php', 'fintech.airtime'
+            __DIR__ . '/../config/airtime.php', 'fintech.airtime'
         );
 
-        $this->app->register(\Fintech\Airtime\Providers\EventServiceProvider::class);
-        $this->app->register(\Fintech\Airtime\Providers\RepositoryServiceProvider::class);
+        $this->app->register(EventServiceProvider::class);
+        $this->app->register(RepositoryServiceProvider::class);
     }
 
     /**
@@ -35,21 +37,21 @@ class AirtimeServiceProvider extends ServiceProvider
         $this->injectOnConfig();
 
         $this->publishes([
-            __DIR__.'/../config/airtime.php' => config_path('fintech/airtime.php'),
+            __DIR__ . '/../config/airtime.php' => config_path('fintech/airtime.php'),
         ]);
 
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
 
-        $this->loadTranslationsFrom(__DIR__.'/../lang', 'airtime');
+        $this->loadTranslationsFrom(__DIR__ . '/../lang', 'airtime');
 
         $this->publishes([
-            __DIR__.'/../lang' => $this->app->langPath('vendor/airtime'),
+            __DIR__ . '/../lang' => $this->app->langPath('vendor/airtime'),
         ]);
 
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'airtime');
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'airtime');
 
         $this->publishes([
-            __DIR__.'/../resources/views' => resource_path('views/vendor/airtime'),
+            __DIR__ . '/../resources/views' => resource_path('views/vendor/airtime'),
         ]);
 
         if ($this->app->runningInConsole()) {
--- src/Services/BangladeshTopUpService.php
+++ src/Services/BangladeshTopUpService.php
@@ -91,7 +91,7 @@ class BangladeshTopUpService
         $data->order_detail_cause_name = 'cash_withdraw';
         $data->order_detail_number = $data->order_data['purchase_number'];
         $data->order_detail_response_id = $data->order_data['purchase_number'];
-        $data->notes = 'Bangladesh Topup Payment Send to '.$master_user_name;
+        $data->notes = 'Bangladesh Topup Payment Send to ' . $master_user_name;
         $orderDetailStore = Transaction::orderDetail()->create(Transaction::orderDetail()->orderDetailsDataArrange($data));
         $orderDetailStore->order_detail_parent_id = $data->order_detail_parent_id = $orderDetailStore->getKey();
         $orderDetailStore->save();
@@ -102,7 +102,7 @@ class BangladeshTopUpService
         $orderDetailStoreForMaster->order_detail_amount = $amount;
         $orderDetailStoreForMaster->converted_amount = $converted_amount;
         $orderDetailStoreForMaster->step = 2;
-        $orderDetailStoreForMaster->notes = 'Bangladesh Topup Payment Receive From'.$user_name;
+        $orderDetailStoreForMaster->notes = 'Bangladesh Topup Payment Receive From' . $user_name;
         $orderDetailStoreForMaster->save();
 
         //For Charge
@@ -110,7 +110,7 @@ class BangladeshTopUpService
         $data->converted_amount = calculate_flat_percent($converted_amount, $serviceStatData['charge']);
         $data->order_detail_cause_name = 'charge';
         $data->order_detail_parent_id = $orderDetailStore->getKey();
-        $data->notes = 'Bangladesh Topup Charge Send to '.$master_user_name;
+        $data->notes = 'Bangladesh Topup Charge Send to ' . $master_user_name;
         $data->step = 3;
         $data->order_detail_parent_id = $orderDetailStore->getKey();
         $orderDetailStoreForCharge = Transaction::orderDetail()->create(Transaction::orderDetail()->orderDetailsDataArrange($data));
@@ -120,7 +120,7 @@ class BangladeshTopUpService
         $orderDetailStoreForChargeForMaster->order_detail_amount = -calculate_flat_percent($amount, $serviceStatData['charge']);
         $orderDetailStoreForChargeForMaster->converted_amount = -calculate_flat_percent($converted_amount, $serviceStatData['charge']);
         $orderDetailStoreForChargeForMaster->order_detail_cause_name = 'charge';
-        $orderDetailStoreForChargeForMaster->notes = 'Bangladesh Topup Charge Receive from '.$user_name;
+        $orderDetailStoreForChargeForMaster->notes = 'Bangladesh Topup Charge Receive from ' . $user_name;
         $orderDetailStoreForChargeForMaster->step = 4;
         $orderDetailStoreForChargeForMaster->save();
 
@@ -128,7 +128,7 @@ class BangladeshTopUpService
         $data->amount = -calculate_flat_percent($amount, $serviceStatData['discount']);
         $data->converted_amount = -calculate_flat_percent($converted_amount, $serviceStatData['discount']);
         $data->order_detail_cause_name = 'discount';
-        $data->notes = 'Bangladesh Topup Discount form '.$master_user_name;
+        $data->notes = 'Bangladesh Topup Discount form ' . $master_user_name;
         $data->step = 5;
         //$data->order_detail_parent_id = $orderDetailStore->getKey();
         //$updateData['order_data']['previous_amount'] = 0;
@@ -139,7 +139,7 @@ class BangladeshTopUpService
         $orderDetailStoreForDiscountForMaster->order_detail_amount = calculate_flat_percent($amount, $serviceStatData['discount']);
         $orderDetailStoreForDiscountForMaster->converted_amount = calculate_flat_percent($converted_amount, $serviceStatData['discount']);
         $orderDetailStoreForDiscountForMaster->order_detail_cause_name = 'discount';
-        $orderDetailStoreForDiscountForMaster->notes = 'Bangladesh Topup Discount to '.$user_name;
+        $orderDetailStoreForDiscountForMaster->notes = 'Bangladesh Topup Discount to ' . $user_name;
         $orderDetailStoreForDiscountForMaster->step = 6;
         $orderDetailStoreForDiscountForMaster->save();
 
@@ -188,7 +188,7 @@ class BangladeshTopUpService
         $data->order_detail_cause_name = 'cash_withdraw';
         $data->order_detail_number = $data->order_data['accepted_number'];
         $data->order_detail_response_id = $data->order_data['purchase_number'];
-        $data->notes = 'Bangladesh Topup Refund From '.$master_user_name;
+        $data->notes = 'Bangladesh Topup Refund From ' . $master_user_name;
         $orderDetailStore = Transaction::orderDetail()->create(Transaction::orderDetail()->orderDetailsDataArrange($data));
         $orderDetailStore->order_detail_parent_id = $data->order_detail_parent_id = $orderDetailStore->getKey();
         $orderDetailStore->save();
@@ -201,7 +201,7 @@ class BangladeshTopUpService
         $orderDetailStoreForMaster->order_detail_amount = -$amount;
         $orderDetailStoreForMaster->converted_amount = -$converted_amount;
         $orderDetailStoreForMaster->step = 2;
-        $orderDetailStoreForMaster->notes = 'Bangladesh Topup Send to '.$user_name;
+        $orderDetailStoreForMaster->notes = 'Bangladesh Topup Send to ' . $user_name;
         $orderDetailStoreForMaster->save();
 
         //For Charge
@@ -209,7 +209,7 @@ class BangladeshTopUpService
         $data->converted_amount = -calculate_flat_percent($converted_amount, $serviceStatData['charge']);
         $data->order_detail_cause_name = 'charge';
         $data->order_detail_parent_id = $orderDetailStore->getKey();
-        $data->notes = 'Bangladesh Topup Charge Receive from '.$master_user_name;
+        $data->notes = 'Bangladesh Topup Charge Receive from ' . $master_user_name;
         $data->step = 3;
         $data->order_detail_parent_id = $orderDetailStore->getKey();
         $orderDetailStoreForCharge = Transaction::orderDetail()->create(Transaction::orderDetail()->orderDetailsDataArrange($data));
@@ -219,7 +219,7 @@ class BangladeshTopUpService
         $orderDetailStoreForChargeForMaster->order_detail_amount = calculate_flat_percent($amount, $serviceStatData['charge']);
         $orderDetailStoreForChargeForMaster->converted_amount = calculate_flat_percent($converted_amount, $serviceStatData['charge']);
         $orderDetailStoreForChargeForMaster->order_detail_cause_name = 'charge';
-        $orderDetailStoreForChargeForMaster->notes = 'Bangladesh Topup Charge Send to '.$user_name;
+        $orderDetailStoreForChargeForMaster->notes = 'Bangladesh Topup Charge Send to ' . $user_name;
         $orderDetailStoreForChargeForMaster->step = 4;
         $orderDetailStoreForChargeForMaster->save();
 
@@ -227,7 +227,7 @@ class BangladeshTopUpService
         $data->amount = calculate_flat_percent($amount, $serviceStatData['discount']);
         $data->converted_amount = calculate_flat_percent($converted_amount, $serviceStatData['discount']);
         $data->order_detail_cause_name = 'discount';
-        $data->notes = 'Bangladesh Topup Discount form '.$master_user_name;
+        $data->notes = 'Bangladesh Topup Discount form ' . $master_user_name;
         $data->step = 5;
         //$data->order_detail_parent_id = $orderDetailStore->getKey();
         $updateData['order_data']['previous_amount'] = 0;
@@ -238,7 +238,7 @@ class BangladeshTopUpService
         $orderDetailStoreForDiscountForMaster->order_detail_amount = -calculate_flat_percent($amount, $serviceStatData['discount']);
         $orderDetailStoreForDiscountForMaster->converted_amount = -calculate_flat_percent($converted_amount, $serviceStatData['discount']);
         $orderDetailStoreForDiscountForMaster->order_detail_cause_name = 'discount';
-        $orderDetailStoreForDiscountForMaster->notes = 'Bangladesh Topup Discount to '.$user_name;
+        $orderDetailStoreForDiscountForMaster->notes = 'Bangladesh Topup Discount to ' . $user_name;
         $orderDetailStoreForDiscountForMaster->step = 6;
         $orderDetailStoreForDiscountForMaster->save();
 
