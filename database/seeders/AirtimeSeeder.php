ca45914ca00d983505245e0d43dd799c1143fb9d
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-08-22 11:34:58 +0600

    type fixing

--- database/seeders/AirtimeSeeder.php
+++ database/seeders/AirtimeSeeder.php
@@ -15,10 +15,10 @@ class AirtimeSeeder extends Seeder
     {
         if (Core::packageExists('Business')) {
             foreach ($this->data() as $entry) {
-                $serviceTypeChild = $entry['serviceTypeChild'] ?? [];
+                $serviceTypeChildren = $entry['serviceTypeChildren'] ?? [];
 
-                if (isset($entry['serviceTypeChild'])) {
-                    unset($entry['serviceTypeChild']);
+                if (isset($entry['serviceTypeChildren'])) {
+                    unset($entry['serviceTypeChildren']);
                 }
 
                 $findServiceTypeModel = Business::serviceType()->list(['service_type_slug' => $entry['service_type_slug']])->first();
@@ -29,8 +29,8 @@ class AirtimeSeeder extends Seeder
                     $serviceTypeModel = Business::serviceType()->create($entry);
                 }
 
-                if (! empty($serviceTypeChild)) {
-                    array_walk($serviceTypeChild, function ($item) use (&$serviceTypeModel) {
+                if (! empty($serviceTypeChildren)) {
+                    array_walk($serviceTypeChildren, function ($item) use (&$serviceTypeModel) {
                         $item['service_type_parent_id'] = $serviceTypeModel->getKey();
                         Business::serviceType()->create($item);
                     });
@@ -45,7 +45,17 @@ class AirtimeSeeder extends Seeder
         $image_png = __DIR__.'/../../resources/img/service_type/logo_png/';
 
         return [
-            ['service_type_parent_id' => null, 'service_type_name' => 'Airtime', 'service_type_slug' => 'air_time', 'logo_svg' => 'data:image/svg+xml;base64,'.base64_encode(file_get_contents($image_svg.'air_time.svg')), 'logo_png' => 'data:image/png;base64,'.base64_encode(file_get_contents($image_png.'air_time.png')), 'service_type_is_parent' => 'yes', 'service_type_is_description' => 'no', 'service_type_step' => '1', 'enabled' => true],
+            [
+                'service_type_parent_id' => null,
+                'service_type_name' => 'Airtime',
+                'service_type_slug' => 'air_time',
+                'logo_svg' => 'data:image/svg+xml;base64,'.base64_encode(file_get_contents($image_svg.'air_time.svg')),
+                'logo_png' => 'data:image/png;base64,'.base64_encode(file_get_contents($image_png.'air_time.png')),
+                'service_type_is_parent' => 'yes',
+                'service_type_is_description' => 'no',
+                'service_type_step' => '1',
+                'enabled' => true
+            ],
         ];
     }
 }
--- database/seeders/BangladeshTopUpSeeder.php
+++ database/seeders/BangladeshTopUpSeeder.php
@@ -20,10 +20,10 @@ class BangladeshTopUpSeeder extends Seeder
             if (! empty($serviceTypes)) {
 
                 foreach ($this->serviceType() as $entry) {
-                    $serviceTypeChild = $entry['serviceTypeChild'] ?? [];
+                    $serviceTypeChildren = $entry['serviceTypeChildren'] ?? [];
 
-                    if (isset($entry['serviceTypeChild'])) {
-                        unset($entry['serviceTypeChild']);
+                    if (isset($entry['serviceTypeChildren'])) {
+                        unset($entry['serviceTypeChildren']);
                     }
 
                     $findServiceTypeModel = Business::serviceType()->list(['service_type_slug' => $entry['service_type_slug']])->first();
@@ -33,8 +33,8 @@ class BangladeshTopUpSeeder extends Seeder
                         $serviceTypeModel = Business::serviceType()->create($entry);
                     }
 
-                    if (! empty($serviceTypeChild)) {
-                        array_walk($serviceTypeChild, function ($item) use (&$serviceTypeModel) {
+                    if (! empty($serviceTypeChildren)) {
+                        array_walk($serviceTypeChildren, function ($item) use (&$serviceTypeModel) {
                             $item['service_type_parent_id'] = $serviceTypeModel->id;
                             Business::serviceType()->create($item);
                         });
@@ -79,7 +79,7 @@ class BangladeshTopUpSeeder extends Seeder
                     'service_type_is_description' => 'no',
                     'service_type_step' => '2',
                     'enabled' => true,
-                    'serviceTypeChild' => [
+                    'serviceTypeChildren' => [
                         [
                             'service_type_name' => 'GrameenPhone',
                             'service_type_slug' => 'grameen_phone_bd',
