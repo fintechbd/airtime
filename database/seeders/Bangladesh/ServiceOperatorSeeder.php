2c0e5f8f18af7ee9c2003b06f37519bec49d5bff
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-09-09 13:19:54 +0600

    error message issue fixed

--- database/seeders/Bangladesh/ServiceOperatorSeeder.php
+++ database/seeders/Bangladesh/ServiceOperatorSeeder.php
@@ -48,6 +48,10 @@ class ServiceOperatorSeeder extends Seeder
                 'service_stat_data' => [
                     'utility_auth_key' => 'VR15208384253881',
                     'utility_secret_key' => 'zKDSu0MJ2qWcRiI8',
+                    'lower_limit' => '0.23',
+                    'higher_limit' => '11.00',
+                    'local_currency_lower_limit' => '20',
+                    'local_currency_higher_limit' => '1000',
                 ],
                 'service_settings' => [
                     'operator_prefix' => '17',
@@ -67,6 +71,10 @@ class ServiceOperatorSeeder extends Seeder
                 'service_stat_data' => [
                     'utility_auth_key' => 'VR15310354624794',
                     'utility_secret_key' => 'MvJhdoFWAcf8ZkEi',
+                    'lower_limit' => '0.23',
+                    'higher_limit' => '11.00',
+                    'local_currency_lower_limit' => '20',
+                    'local_currency_higher_limit' => '1000',
                 ],
                 'service_settings' => [
                     'operator_prefix' => '19',
@@ -86,6 +94,10 @@ class ServiceOperatorSeeder extends Seeder
                 'service_stat_data' => [
                     'utility_auth_key' => 'VR15310354922071',
                     'utility_secret_key' => 'fXJ0gotvoVHnyY3M',
+                    'lower_limit' => '0.23',
+                    'higher_limit' => '11.00',
+                    'local_currency_lower_limit' => '20',
+                    'local_currency_higher_limit' => '1000',
                 ],
                 'service_settings' => [
                     'operator_prefix' => '18',
@@ -93,7 +105,6 @@ class ServiceOperatorSeeder extends Seeder
                     'operator_short_code' => '3',
                 ],
             ],
-
             [
                 'service_type_name' => 'Teletalk',
                 'service_type_slug' => 'teletalk_bd',
@@ -106,6 +117,10 @@ class ServiceOperatorSeeder extends Seeder
                 'service_stat_data' => [
                     'utility_auth_key' => 'VR15310355096848',
                     'utility_secret_key' => 'szbrpaBFunmpNBZ0',
+                    'lower_limit' => '0.23',
+                    'higher_limit' => '11.00',
+                    'local_currency_lower_limit' => '20',
+                    'local_currency_higher_limit' => '1000',
                 ],
                 'service_settings' => [
                     'operator_prefix' => '15',
@@ -125,6 +140,10 @@ class ServiceOperatorSeeder extends Seeder
                 'service_stat_data' => [
                     'utility_auth_key' => 'VR15310354477640',
                     'utility_secret_key' => 'fyj8yjmgvJlI9ou8',
+                    'lower_limit' => '0.23',
+                    'higher_limit' => '11.00',
+                    'local_currency_lower_limit' => '20',
+                    'local_currency_higher_limit' => '1000',
                 ],
                 'service_settings' => [
                     'operator_prefix' => '16',
@@ -144,6 +163,10 @@ class ServiceOperatorSeeder extends Seeder
                 'service_stat_data' => [
                     'utility_auth_key' => '',
                     'utility_secret_key' => '',
+                    'lower_limit' => '0.23',
+                    'higher_limit' => '11.00',
+                    'local_currency_lower_limit' => '20',
+                    'local_currency_higher_limit' => '1000',
                 ],
                 'service_settings' => [
                     'operator_prefix' => '13',
