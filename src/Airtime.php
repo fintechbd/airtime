99d7cc2c6692f72f7e90fd8407e8206c43b7e80a
Mohammad Hafijul Islam <hafijul233@gmail.com>
2025-05-26 17:22:53 +0600

    phpdoc block updated

--- src/Airtime.php
+++ src/Airtime.php
@@ -5,19 +5,33 @@ namespace Fintech\Airtime;
 use Fintech\Airtime\Services\AssignVendorService;
 use Fintech\Airtime\Services\BangladeshTopUpService;
 use Fintech\Airtime\Services\InternationalTopUpService;
+use Fintech\Core\Abstracts\BaseModel;
+use Illuminate\Database\Eloquent\Collection;
 
 class Airtime
 {
+    /**
+     * @param $filters
+     * @return BangladeshTopUpService|Collection|BaseModel
+     */
     public function bangladeshTopUp($filters = null)
     {
         return \singleton(BangladeshTopUpService::class, $filters);
     }
 
+    /**
+     * @param $filters
+     * @return InternationalTopUpService|Collection|BaseModel
+     */
     public function internationalTopUp($filters = null)
     {
         return \singleton(InternationalTopUpService::class, $filters);
     }
 
+    /**
+     * @param $filters
+     * @return AssignVendorService
+     */
     public function assignVendor()
     {
         return \app(AssignVendorService::class);
